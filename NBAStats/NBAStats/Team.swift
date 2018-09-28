713bd409895924f2054dc01b99314130dd86248d
Andrew Hwang <andrewhwang@Andrews-MacBook-Pro.local>
2018-09-28 16:43:58 -0400

    Added Team Cell file

---
+++ NBAStats/NBAStats/Team.swift
@@ -0,0 +1,13 @@
+//
+//  Team.swift
+//  NBAStats
+//
+//  Created by Andrew Hwang on 9/28/18.
+//  Copyright © 2018 WhiteHaus. All rights reserved.
+//
+
+import UIKit
+
+class Team: UICollectionViewCell {
+    
+}
--- NBAStats/NBAStats/ViewController.swift
+++ NBAStats/NBAStats/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    let teams = ["Bulls", "Wizards"]
+    var teamsCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
+    let reuseID = "teamCell"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return teams.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseID, for: indexPath)
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+    }
+    
+    func loadData() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: view.frame.width, height: 100)
+        
+        teamsCollectionView = UICollectionView(frame: CGRect(x: view.frame.width/2, y: view.frame.height/2, width: view.frame.width, height: view.frame.height), collectionViewLayout: layout)
+        teamsCollectionView.delegate = self
+        teamsCollectionView.dataSource = self
+        
     }
 
 
